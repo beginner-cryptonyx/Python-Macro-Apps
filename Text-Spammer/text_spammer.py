3e6e2bbd6a1a15ca6e5b1ddc2db535cf2bfbad96
Crypto <mayanksethi242008@gmail.com>
2021-11-26 00:49:21 +0530

    text spammer added

---
+++ Text-Spammer/text_spammer.py
@@ -0,0 +1,29 @@
+import pyautogui
+import win32clipboard
+
+class spammer():
+    def __init__(self, text="", count=1):
+        self.count = count
+
+        if text == "":
+            win32clipboard.OpenClipboard()
+            self.text = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+        else:
+            self.text = text
+
+    def Paste(self):
+        count = int(self.count)
+        while count != 0:
+            text = str(self.text)
+            pyautogui.typewrite(text)
+            pyautogui.press('enter')
+            count -= 1
+
+if "__main__" == __name__:
+    text = input("Enter Text To Paste "
+    "(If Empty Will Paste ClipBoard): ")
+    count = input("How Many Times To Paste: ")
+
+    bot = spammer(text, count)
+    bot.Paste()
---
+++ Text-Spammer/text_spammer_file.py
@@ -0,0 +1,23 @@
+import pyautogui
+import sys, os
+import termcolor
+
+
+class spammer():
+    def __init__(self, path="Text-Spammer/file.txt"):
+
+        if os.path.exists(path):
+            self.path = path
+        else:
+            print(termcolor.colored(("FILE/PATH INVALID"), 'red'))
+            sys.exit(0)
+
+    def Paste(self):
+        file = open(self.path, 'r')
+        for line in file:
+            pyautogui.typewrite(line)
+
+if "__main__" == __name__:
+
+    bot = spammer()
+    bot.Paste()
